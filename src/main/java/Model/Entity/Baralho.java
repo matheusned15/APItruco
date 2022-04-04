531653776ea19c6bc2a6a388f595150914583eca
Matheus.Ned <matheusned15@gmail.com>
2022-04-04 17:52:07 -0300

    Criação de métodos de manipulação da classe baralho

--- src/main/java/Model/Entity/Baralho.java
+++ src/main/java/Model/Entity/Baralho.java
@@ -1,16 +1,60 @@
 package Model.Entity;
 
 import javax.persistence.*;
+import javax.smartcardio.Card;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class Baralho {
 
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private List<Carta> cartas = new ArrayList<>();
+    private Random randomGenerator = new Random();
 
-    @OneToMany
-    private final ArrayList<Carta> utilizadas = new ArrayList<>();
 
+    public Baralho() {
+        // Create new cards
+        for (Rank rank : Rank.values()) {
+            for (NAIPE naipe : NAIPE.values()) {
+                Carta carta = new Carta(rank, naipe, false);
+                this.cartas.add(carta);
+            }
+        }
+    }
+
+
+    public List<Carta> getCards() { return cartas; }
+
+    public void setCards(ArrayList<Carta> _deck) { this.cartas = _deck; }
+
+    public Carta drawRandomCard() {
+        int index = this.randomGenerator.nextInt(cartas.size());
+        Carta card = this.cartas.get(index);
+        //System.out.println("Card Picked: " + card.toString());
+        cartas.remove(index);
+        return card;
+    }
+
+    public Carta drawTopCard() {
+        Carta card = this.cartas.get(0);
+        cartas.remove(0);
+        System.out.println("Card Picked: " + card.toString());
+        return card;
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(this.cartas);
+    }
+
+    public void resetDeck() {
+        this.cartas.clear();
+        //Create new cards
+        for (Rank rank : Rank.values()) {
+            for (NAIPE naipe : NAIPE.values()) {
+                Carta card = new Carta(rank, naipe, false);
+                this.cartas.add(card);
+            }
+        }
+    }
 }
